ba643475788c9bbc3baf55115390386dbb5aca93
Axel <julian_s_99@live.com>
2022-09-28 16:59:15 -0300

    db-env

--- index.js
+++ index.js
@@ -10,6 +10,11 @@ app.get("/env", function (req, res) {
         environment: process.env.NODE_ENV
     });
 });
+app.get("/db-env", function (req, res) {
+    res.json({
+        "db-host": process.env.DB_HOST
+    });
+});
 app.get("/hola", function (req, res) {
     res.json({
         message: "Hola? XD, my name is heroku"
--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 import * as express from "express";
+import "dotenv/config";
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -7,6 +8,11 @@ console.log(process.env.NODE_ENV);
 app.get("/env", (req, res) => {
   res.json({
     environment: process.env.NODE_ENV,
+  });
+});
+app.get("/db-env", (req, res) => {
+  res.json({
+    "db-host": process.env.DB_HOST,
   });
 });
 app.get("/hola", (req, res) => {
